52621740f96a87648c10353d3716d3f8dd45eb5d
Amit Goswami <cseiubatgoswami@gmail.com>
2021-02-27 08:42:03 +0600

    recruiter_Design

--- job/urls.py
+++ job/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('contact/', views.contact, name="contact"),
     path('admin_login/', views.admin_login, name="admin_login"),
+    path('recruiter_login/', views.recruiter_login, name="recruiter_login"),
 ]
--- job/views.py
+++ job/views.py
@@ -19,3 +19,6 @@ def contact(request):
 
 def admin_login(request):
     return render(request, 'job/admin_login.html',context={})
+
+def recruiter_login(request):
+    return render(request, 'job/recruiter_login.html', context={})
