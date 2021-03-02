7015d0188d78be3e987c7d0fba5ac2577800b2a6
Amit Goswami <cseiubatgoswami@gmail.com>
2021-03-03 00:12:06 +0600

    recruiter_home

--- Recruiters_Login/urls.py
+++ Recruiters_Login/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('recruiter_login/', views.recruiter_login, name="recruiter_login"),
     path('recruiter_home/', views.recruiter_home, name="recruiter_home"),
     path('recruiter_logout/', views.recruiter_logout, name="recruiter_logout"),
+    path('requiterChangePass/', views.requiterChangePass, name="requiterChangePass"),
+    path('add_job/', views.add_job, name="add_job"),
+    path('job_list/', views.job_list, name="job_list"),
+    path('edit_joblist/<int:pk>/', views.edit_joblist, name="edit_joblist"),
+    path('delete_joblist/<int:pk>/', views.delete_joblist, name="delete_joblist"),
+    path('change_companyLogo/<int:pk>/', views.change_companyLogo, name="change_companyLogo"),
 ]
